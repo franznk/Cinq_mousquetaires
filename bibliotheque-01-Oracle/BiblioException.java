3c307308ecdfe46a718510b010fa9d40513882e9
Franz Nkezimana <Franz.Nkezimana.1@collegeahuntsic.qc.ca>
2015-09-16 12:19:08 -0400

    Bibliotheque-01-Oracle

---
+++ bibliotheque-01-Oracle/BiblioException.java
@@ -0,0 +1,10 @@
+/**
+ * L'exception BiblioException est lev�e lorsqu'une transaction est inad�quate.
+ * Par exemple -- livre inexistant
+ */
+
+public final class BiblioException extends Exception {
+    public BiblioException(String message) {
+        super(message);
+    }
+}
